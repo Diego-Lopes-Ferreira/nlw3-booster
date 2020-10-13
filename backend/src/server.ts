135907a0c9c6c534f7384c60d139a861e2275889
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-10-13 08:41:09 -0300

    chore: add and configure typescript

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,4 +3,9 @@ import express from 'express';
 // * Happy app
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({message: 'Oi'})
+  console.log('Hello, world!!')
+});
+
 app.listen(3333);
