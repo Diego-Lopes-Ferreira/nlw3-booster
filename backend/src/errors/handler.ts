b24c955a04e7bc081434d0dd028850c466952afe
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-10-13 20:45:04 -0300

    feat: add Yup validation Error handling

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,8 +1,28 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+  if (error instanceof ValidationError) {
+    console.log('Validation error');
+
+    let errors: ValidationErrors = {};
+
+    error.inner.forEach((err) => {
+      errors[err.path] = err.errors;
+    });
+
+    return response.status(400).json({
+      message: 'Validation error',
+      errors,
+    });
+  }
   console.log('Internal Server error');
   console.error(error);
+
   return response.status(500).json({
     message: 'internal server error',
     error_log: error,
