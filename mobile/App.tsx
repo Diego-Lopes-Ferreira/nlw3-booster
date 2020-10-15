dd40adfc1b7b8a3743eb1b81569e879fba39d08b
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-10-15 09:04:49 -0300

    feat: implemented Map

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import MapPage from './pages/MapPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text style={styles.text}>Hello, NLW #3!</Text> */}
+      <MapPage />
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
   },
 });
---
+++ mobile/pages/MapPage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
+import MapView from 'react-native-maps';
+
+
+// -22.9955881,-49.8649668
+export default function MapPage() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -22.9955881,
+          longitude: -49.8649668,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    marginTop: Constants.statusBarHeight + 10,
+  },
+});
