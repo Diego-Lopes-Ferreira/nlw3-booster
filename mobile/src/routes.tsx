1578f1b8a20ee05ab3b28bd9013d110e70c8c7f5
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-10-15 14:04:49 -0300

    feat: card style interpolators

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
 import MapPage from './pages/MapPage';
 import OrphanageDetail from './pages/OrphanageDetail';
@@ -15,6 +15,33 @@ function Routes() {
       <Navigator
         screenOptions={{
           headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          transitionSpec: {
+            open: {
+              animation: 'spring',
+              config: {
+                stiffness: 1000,
+                damping: 500,
+                mass: 3,
+                overshootClamping: true,
+                restDisplacementThreshold: 10,
+                restSpeedThreshold: 10,
+              },
+            },
+            close: {
+              animation: 'spring',
+              config: {
+                stiffness: 1000,
+                damping: 500,
+                mass: 3,
+                overshootClamping: true,
+                restDisplacementThreshold: 10,
+                restSpeedThreshold: 10,
+              },
+            },
+          }
         }}
       >
         <Screen
